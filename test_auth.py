b0266c8729bfa5d7437ce5773dc55539e8eb6287
muxazzz <60567035+muxazzz@users.noreply.github.com>
2020-07-26 13:11:02 +0300

    Update test_auth.py

--- test_auth.py
+++ test_auth.py
@@ -18,6 +18,13 @@ def test_get_all_tasks_not_empty():
     
 def setup_function(function):
     print("setting up", function)
+    
+def test_func1():
+    assert True
+
+
+def test_func2():
+    assert False
 
 if __name__ == '__main__':
     unittest.main()
