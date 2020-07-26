c689c9168e72009c169140c354a82ffd2c755cc6
muxazzz <60567035+muxazzz@users.noreply.github.com>
2020-07-26 13:13:13 +0300

    Update test_auth.py

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -18,6 +18,24 @@ def test_get_all_tasks_not_empty():
 
 def setup_function(function):
     print("setting up", function)
+    
+def test_func1():
+    assert True
+
+
+def test_func2():
+    assert False
+
+    
+def test_myoutput(capsys):  # or use "capfd" for fd-level
+    print("hello")
+    sys.stderr.write("world\n")
+    captured = capsys.readouterr()
+    assert captured.out == "hello\n"
+    assert captured.err == "world\n"
+    print("next")
+    captured = capsys.readouterr()
+    assert captured.out == "next\n"
 
 if __name__ == '__main__':
     unittest.main()
