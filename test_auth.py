6a5f691310384c8cb95dda3241ae0d6d4affe52c
muxazzz <60567035+muxazzz@users.noreply.github.com>
2020-07-26 13:11:32 +0300

    Update test_auth.py

--- test_auth.py
+++ test_auth.py
@@ -26,5 +26,16 @@ def test_func1():
 def test_func2():
     assert False
 
+    
+def test_myoutput(capsys):  # or use "capfd" for fd-level
+    print("hello")
+    sys.stderr.write("world\n")
+    captured = capsys.readouterr()
+    assert captured.out == "hello\n"
+    assert captured.err == "world\n"
+    print("next")
+    captured = capsys.readouterr()
+    assert captured.out == "next\n"
+    
 if __name__ == '__main__':
     unittest.main()
